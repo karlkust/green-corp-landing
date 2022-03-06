eb225424ef10fba60a5721247f0c85c6e44122d7
KarlKust <zklizard@gmail.com>
2022-03-06 16:10:57 +0500

    Финал

--- js/animations.js
+++ js/animations.js
@@ -1,12 +1,47 @@
 const INCREASE_NUMBER_ANIMATION_SPEED = 50;
 
-function increaseNumberAnimationStep(i, element, endNumber){
-    if (i <= endNumber){
-        if (i === endNumber){
-            element.innerText = i + "+";
-        } else {
-            element.innerText = i;
-        }
-        i++;
+// function increaseNumberAnimationStep(i, element, endNumber) {
+//     if (i <= endNumber) {
+//         if (i === endNumber) {
+//             element.innerText = i + "+";
+//         } else {
+//             element.innerText = i;
+//         }
+//         i++;
+//     }
+// }
+
+document.querySelector('#budget').addEventListener('change', function handleSelectChange(event) {
+    if (event.target.value === 'other') {
+        const formContainer = document.createElement('div');
+        formContainer.classList.add('form__group');
+        formContainer.classList.add('form__other-input');
+
+        const input = document.createElement('input');
+        input.placeholder = "Введите ваш вариант";
+        input.type = "text";
+
+        formContainer.appendChild(input);
+        document.querySelector('.form form').insertBefore(formContainer, document.querySelector('.form__submit'));
+    }
+    if (event.target.value !== 'other' && otherInput) {
+        const otherInput = document.querySelector('.form__other-input');
+        document.querySelector('.form form').removeChild(otherInput);
+    }
+});
+
+function updateScroll() {
+    if (window.scrollY > 0) {
+        document.querySelector('header').classList.add('header__scrolled');
+    } else {
+        document.querySelector('header').classList.remove('header__scrolled');
+    }
+    let windowBottomPosition = window.scrollY + window.innerHeight;
+    let countElementPosition = document.querySelector('.features__clients-count').offsetTop;
+    if (windowBottomPosition >= countElementPosition && !animationInited) {
+        animationInited = true;
+        initIncreaseNumberAnimation();
     }
 }
+
+window.addEventListener('scroll', updateScroll);
